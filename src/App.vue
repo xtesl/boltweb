095a69b5a8833b0fa7b535cca384d7edfebdc769
Immanuel <aimmanuel925@gmail.com>
2025-12-22 13:38:40 +0000

    Add WhatsApp button

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@
     <RouterView />
       <Footer />
   </main>
+  <WhatsAppButton 
+      phone-number="233501234567"
+      message="Hello! I'd like to know more"
+      :show-pulse="true"
+    />
 </template>
 
 <script setup>
 import MainNavbar from "./components/MainNavbar.vue"
 import Footer from './components/Footer.vue'
+import WhatsAppButton from './components/WhatsAppButton.vue'
 </script>
 
 
