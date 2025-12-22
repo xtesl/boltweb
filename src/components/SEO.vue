782f0fd9feb389f330133cee0ef7b9eb23a37c53
Immanuel <aimmanuel925@gmail.com>
2025-12-22 14:48:36 +0000

    Initiate SEO optimization processes

---
+++ src/components/SEO.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { useHead } from '@vueuse/head'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'BoltWeb'
+  },
+  description: {
+    type: String,
+    default: 'BoltWeb delivers fast websites for business in no time. Get your business online in small time with few cedis.'
+  },
+  image: {
+    type: String,
+    default: '/images/boltweb_logo.png' // Default Open Graph image
+  },
+  url: {
+    type: String,
+    default: 'https://boltweb.onrender.com'
+  },
+  type: {
+    type: String,
+    default: 'website'
+  }
+})
+
+useHead({
+  title: props.title,
+  meta: [
+    // Basic Meta Tags
+    { name: 'description', content: props.description },
+    { name: 'keywords', content: 'Fast, Affordable, Website, ' },
+    { name: 'author', content: 'Emmanuel' },
+    
+    // Open Graph / Facebook
+    { property: 'og:title', content: props.title },
+    { property: 'og:description', content: props.description },
+    { property: 'og:image', content: props.image },
+    { property: 'og:url', content: props.url },
+    { property: 'og:type', content: props.type },
+    { property: 'og:site_name', content: 'BoltWeb' },
+    
+    // Twitter Card
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:title', content: props.title },
+    { name: 'twitter:description', content: props.description },
+    { name: 'twitter:image', content: props.image },
+    { name: 'twitter:site', content: '@manuel_tamme' },
+    
+    // WhatsApp (uses Open Graph)
+    { property: 'og:image:width', content: '1200' },
+    { property: 'og:image:height', content: '630' },
+  ],
+  link: [
+    { rel: 'canonical', href: props.url }
+  ]
+})
+</script>
+
+<template>
+  <div></div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
 import './style.css'
 import router from './router'
 import App from './App.vue'
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
+const head = createHead()
+
 app.use(router)
+app.use(head)
 app.mount('#app')
